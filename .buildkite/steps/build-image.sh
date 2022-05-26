902662fcb0d5df5bcd48136f5ccbaf484f26d3ef
Ivan Loboda <lobodatell@yandex.ru>
2022-05-26 17:40:09 +0300

    deploy schema to DB

--- .buildkite/steps/build-image.sh
+++ .buildkite/steps/build-image.sh
@@ -4,3 +4,4 @@ REVISION=${BUILDKITE_COMMIT:=$(git rev-parse HEAD)}
 echo "REVISION: $REVISION"
 
 docker build -t neonlabsorg/solana-accountsdb:${REVISION} .
+docker build -t neonlabsorg/accountsdb:${REVISION} ./scripts
